2b7a9a9a20807a8f9653cb9e0c0bd9aaf63a5d2b
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-07-09 17:47:10 -0500

    MVP

--- index.js
+++ index.js
@@ -1,31 +1,99 @@
-// implement your API here
+const express = require("express");
+// express = lightweight
+// routers -> organizing our endpoints
+// middleware -> allows us to expand and customize
 
-// require the express npm module, needs to be added to the project using "yarn add" or "npm install"
-// use require() to import the express module and make it available to our application.
-// const express = require('express'); is equivalent to import express from 'express'; if we were using ES2015 syntax.
-const express = require('express');
-
-const Users = require('./data/db.js');
+const db = require('./data/db.js');
 
 const server = express();
+//const { hubs } = db;
 
-server.use(express.json());
+// creating endpoints
+// I want to make something available in case anyone needs it
+server.use(express.json()); // makes post and put work
+  //console.log('inside the get request');
+  // specify a data type
+  // set a status code
+  // send a response
+  server.get("/", (req, res) => {
+    res.send("<h2>Hello World</h2>");
+  });
 
-// configures our server to execute a function for every GET request to "/"
-// the second argument passed to the .get() method is the "Route Handler Function"
-// the route handler function will run on every GET request to "/"
 
-server.get('/', (req,res) => {
-// express will pass the request and response objects to this function
-// the .send() on the response object can be used to send a response to the client
-    res.send('Hello World!')
+
+//Create
+server.post("/users", (req, res) => {
+  
+  const newUser = req.body;
+ 
+  db.insert(newUser)
+   
+    .then(user => {
+      res.status(201).json(user); /*created*/
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error while saving the user to the database" })
+    });
+    
 });
 
+//Read
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      //handle error
+      res.status(500).json({ error: "The users information could not be retrieved." });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const {id} = req.params /* using id you have to define it first*/
+    db.findById(id) /* be sure you are running the correct function found in db.js*/
+      .then(id => {  /* if successful run this */
+        res.status(200).json(id);
+      })
+      .catch(err => { /*if not successful run this instead*/
+        //handle error
+        res.status(500).json({ error: "The users information could not be retrieved." });
+      });
+  });
 
 
+//Delete
+server.delete('/api/users/:id', (req, res) => {
+  const {id} = req.params;
+
+  db.remove(id)
+    .then(deleted => {
+      res.status(204).end(); //end tells client that request is finished.
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'error deleting id' });
+    });
+});
+
+//Update
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const {name, bio} = req.body
+    if(!name|| !bio) {
+      res.status(404).json({ error: "Name and Bio required" });
+    }else {
+    db.update(req.params.id, req.body )
+    .then(updated => {
+      if (updated) {
+        res.status(200).json(updated);
+      } 
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error while saving the user to the database" });
+    })}});
 
 
-// once the server is fully configured we can have it "listen" for connections on a particular "port"
-// the callback function passed as the second argument will run once when the server starts
-
-server.listen(8000, () => console.log('API running on port 8000'));
+// listening
+server.listen(8000, () => {
+  console.log('Listening on port 8000');
+});
